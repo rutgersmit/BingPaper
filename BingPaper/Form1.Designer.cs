445a8399116c059721f2fef05cd4f08b519321aa
Rutger <rutger.smit@gmail.com>
2020-08-25 09:42:33 +0200

    Add project files.

---
+++ BingPaper/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace BingPaper
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+			this.components = new System.ComponentModel.Container();
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.buttonOk = new System.Windows.Forms.Button();
+			this.buttonCancel = new System.Windows.Forms.Button();
+			this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+			this.comboBox1 = new System.Windows.Forms.ComboBox();
+			this.label1 = new System.Windows.Forms.Label();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(800, 460);
+			this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.pictureBox1.TabIndex = 0;
+			this.pictureBox1.TabStop = false;
+			this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
+			// 
+			// buttonOk
+			// 
+			this.buttonOk.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+			this.buttonOk.Location = new System.Drawing.Point(737, 478);
+			this.buttonOk.Name = "buttonOk";
+			this.buttonOk.Size = new System.Drawing.Size(75, 75);
+			this.buttonOk.TabIndex = 1;
+			this.buttonOk.Text = "&OK";
+			this.buttonOk.UseVisualStyleBackColor = true;
+			this.buttonOk.Click += new System.EventHandler(this.buttonOk_Click);
+			// 
+			// buttonCancel
+			// 
+			this.buttonCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+			this.buttonCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+			this.buttonCancel.Location = new System.Drawing.Point(12, 478);
+			this.buttonCancel.Name = "buttonCancel";
+			this.buttonCancel.Size = new System.Drawing.Size(75, 75);
+			this.buttonCancel.TabIndex = 1;
+			this.buttonCancel.Text = "&Cancel";
+			this.buttonCancel.UseVisualStyleBackColor = true;
+			this.buttonCancel.Click += new System.EventHandler(this.buttonCancel_Click);
+			// 
+			// comboBox1
+			// 
+			this.comboBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 43F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.comboBox1.FormattingEnabled = true;
+			this.comboBox1.Items.AddRange(new object[] {
+            "en-AU",
+            "jp-JP"});
+			this.comboBox1.Location = new System.Drawing.Point(93, 518);
+			this.comboBox1.Name = "comboBox1";
+			this.comboBox1.Size = new System.Drawing.Size(638, 73);
+			this.comboBox1.TabIndex = 2;
+			this.comboBox1.Visible = false;
+			this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+			// 
+			// label1
+			// 
+			this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.Location = new System.Drawing.Point(94, 478);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(637, 75);
+			this.label1.TabIndex = 3;
+			this.label1.Text = "label1";
+			this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// Form1
+			// 
+			this.AcceptButton = this.buttonOk;
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.CancelButton = this.buttonCancel;
+			this.ClientSize = new System.Drawing.Size(824, 566);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.comboBox1);
+			this.Controls.Add(this.buttonCancel);
+			this.Controls.Add(this.buttonOk);
+			this.Controls.Add(this.pictureBox1);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.KeyPreview = true;
+			this.Name = "Form1";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "Bing Wallpaper";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button buttonOk;
+        private System.Windows.Forms.Button buttonCancel;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.ComboBox comboBox1;
+		private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ BingPaper/Form1.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace BingPaper
+{
+
+	public partial class Form1 : Form
+	{
+		private int currentIndex = 0;
+		List<BingImage> bingImages = new List<BingImage>();
+
+		public Form1(string[] args)
+		{
+			InitializeComponent();
+
+			if (args != null && args.Length == 1 && args[0] == "set")
+			{
+				GetWallpapers();
+				SetWallpaper();
+				Application.Exit();
+				return;
+			}
+		}
+
+		private const int SPI_SETDESKWALLPAPER = 20;
+		private const int SPIF_UPDATEINIFILE = 0x01;
+		private const int SPIF_SENDWININICHANGE = 0x02;
+
+		[DllImport("user32.dll", CharSet = CharSet.Auto)]
+		private static extern int SystemParametersInfo(int uAction, int uParam, string lpvParam, int fuWinIni);
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			GetWallpapers();
+		}
+
+		private void GetWallpapers(string region = "en-ww")
+		{
+			try
+			{
+				string address = "http://www.bing.com/hpimagearchive.aspx?format=xml&idx=0&n=20&mbl=1&mkt=" + region;
+				string text = "";
+
+				using (WebClient webClient = new WebClient())
+					text = webClient.DownloadString(address);
+
+				var doc = XDocument.Parse(text).Descendants("image");
+				foreach (var n in doc)
+				{
+					bingImages.Add(new BingImage()
+						{
+							Copyright = Convert(n.Descendants("copyright").FirstOrDefault().Value), 
+							ImageUrl = n.Descendants("url").FirstOrDefault().Value
+						});
+
+				}
+
+				ShowBingImage(0);
+			}
+			catch (Exception)
+			{
+				if (MessageBox.Show("An error occurred getting the current Bing background.\r\nPlease try again later.", "Error", MessageBoxButtons.RetryCancel, MessageBoxIcon.Error) == DialogResult.Retry)
+					GetWallpapers(region);
+			}
+		}
+
+		private void SetWallpaper()
+		{
+			if (File.Exists(System.IO.Path.GetTempPath() + "\\bingwallpaper.jpg"))
+				SystemParametersInfo(SPI_SETDESKWALLPAPER, 0, System.IO.Path.GetTempPath() + "\\bingwallpaper.jpg", SPIF_UPDATEINIFILE | SPIF_SENDWININICHANGE);
+		}
+		
+		private void buttonOk_Click(object sender, EventArgs e)
+		{
+			SetWallpaper();
+			this.Close();
+		}
+
+		private void buttonCancel_Click(object sender, EventArgs e)
+		{
+			this.Close();
+		}
+
+		private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+		{
+			GetWallpapers(comboBox1.Text);
+		}
+
+		private void ShowBingImage(int index)
+		{
+			if (index < 0 || index > bingImages.Count - 1)
+				return;
+
+			using (WebClient webClient = new WebClient())
+			{
+				((NameValueCollection)webClient.Headers).Add("User-agent", "BingDesktop");
+				webClient.DownloadFile("http://www.bing.com" + bingImages[index].ImageUrl + "_1920x1200.jpg", Path.GetTempPath() + "\\bingwallpaper.jpg");
+			}
+
+			pictureBox1.ImageLocation = Path.GetTempPath() + "\\bingwallpaper.jpg";
+
+			var note =  bingImages[index].Copyright;
+			if (note.Contains("("))
+				note = note.Insert(note.IndexOf("("), Environment.NewLine);
+
+			label1.Text = note; 
+			toolTip1.SetToolTip(pictureBox1, note);
+		}
+
+		private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+		{
+			if (e.X < pictureBox1.Width/2)
+			{
+				//currentIndex = currentIndex + 1;
+				currentIndex++;
+				if (currentIndex > bingImages.Count - 1)
+					currentIndex--;
+				ShowBingImage(currentIndex);
+			}
+			else
+			{
+				currentIndex--;
+				if (currentIndex < 0)
+					currentIndex = 0;
+				ShowBingImage(currentIndex);
+			}
+		}
+
+
+		private string Convert(string input)
+		{
+			byte[] data = Encoding.Default.GetBytes(input);
+			return Encoding.UTF8.GetString(data);
+		}
+
+	}
+
+	internal class BingImage
+	{
+		public string Copyright { get; set; }
+		public string ImageUrl { get; set; }
+	}
+
+}
